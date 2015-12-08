15019e2b95ff050876fba09ff68e4a56e07628b4
Matt Ewell <matt.ewell@gmail.com>
2015-12-07 20:36:53 -0700

    Basic play working
    Add neighbor find and update functions to board.
    Add call to update neighbors from the light click
    listener. Create board file and move board object
    constructor to it.

---
+++ board.js
@@ -0,0 +1,48 @@
+function Board() {
+  this.width = 5;
+  this.element = $('.board');
+  this.initLightGrid();
+
+}
+
+Board.prototype.initLightGrid = function(){
+  this.lightGrid = new Array(this.width);
+  for(var i = 0; i < this.width; i++){
+    this.lightGrid[i] = new Array(this.width);
+  }
+
+  for(var i = 0; i < this.width; i++){
+    for(var j = 0; j < this.width; j++){
+      this.lightGrid[i][j] = new Light(i, j, this);
+    }
+  }
+};
+
+Board.prototype.onBoard = function(x,y){
+  return ((x < this.width && y < this.width) &&
+          (x >= 0 && y >= 0));
+}
+
+Board.prototype.rawNeighborCoords = function(x,y){
+  var raw = [[x - 1, y], [x, y - 1], [x + 1, y], [x, y + 1]];
+  return raw.filter(function(coords){
+    return this.onBoard(coords[0], coords[1]);
+  }.bind(this));
+}
+
+Board.prototype.getNeighbors = function(light){
+  return this.rawNeighborCoords(light.x, light.y).map(function(pair){
+    return this.lightGrid[pair[0]][pair[1]];
+  }.bind(this));
+}
+
+Board.prototype.updateNeighbors = function(light){
+  this.getNeighbors(light).forEach(function(light){
+    light.flipSwitch().rerender();
+  }.bind(this));
+}
+
+Board.prototype.countLights = function()
+{
+  return this.element.find('.lit').length;
+}
---
+++ light.js
@@ -0,0 +1,61 @@
+function Light(x, y, board) {
+  this.board = board;
+  this.x = x;
+  this.y = y;
+  this.lit = false;
+
+  this.render().bindClick().appendTo(this.board.element);
+}
+
+Light.prototype.render = function() {
+  this.element = $(this.template());
+  return this;
+};
+
+Light.prototype.rerender = function() {
+  this.element.replaceWith(this.render().bindClick().element);
+  return this;
+};
+
+Light.prototype.bindClick = function() {
+  this.element.find('.light')
+  .on('click', function () {
+    this.flipSwitch().rerender();
+    this.board.updateNeighbors(this);
+  }.bind(this));
+
+  return this;
+};
+
+Light.prototype.flipSwitch = function() {
+  if(this.lit) {
+    this.lit = false;
+  } else {
+    this.lit = true;
+  }
+  return this;
+}
+
+Light.prototype.template = function() {
+  if (this.lit) {
+    return `
+      <div class="cell">
+        <div class="light lit">
+        </div>
+      </div>
+    `;
+  } else {
+    return `
+      <div class="cell">
+        <div class="light">
+        </div>
+      </div>
+
+    `
+  }
+}
+
+Light.prototype.appendTo = function(target) {
+  this.element.appendTo(target);
+  return this;
+}
