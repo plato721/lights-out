7db7c8f26e0698ce236c6f024fbdffdc0425890e
Matt Ewell <matt.ewell@gmail.com>
2015-12-07 19:22:52 -0700

    Add OO light
    Board still responsive, but now light objects are created
    as the non-OO board iterates through its 2-d array

--- lights-out.js
+++ lights-out.js
@@ -4,28 +4,13 @@ function main(){
   // this.width = 5;
   // this.lightGrid = Array.new(width);
   // this.container = $('.board');
+  this.element = $('.board');
 
     for(var i = 0; i < 5; i++){
       for(var j = 0; j < 5; j++){
-        // var light = new Light(i, j, board);
-        $(renderLight())
-          .find('.light')
-          .on('click', function () {
-            $(this).toggleClass('lit');
-          })
-          .end()
-          .appendTo($('.board'));
+        new Light(i, j, this);
       }
     }
-  }
-
-  function renderLight(){  //temporary for testing
-    return `
-      <div class="cell">
-        <div class="light">
-        </div>
-      </div>
-    `;
   }
 
   // Board.prototype.countLights = function()
@@ -33,23 +18,75 @@ function main(){
 
   // }
 
-  // function Light(x, y, board) {
-  //   this.board = board;
-  //   this.x = x;
-  //   this.y = y;
-  // }
+  function Light(x, y, board) {
+    this.board = board.element;
+    this.x = x;
+    this.y = y;
+    this.lit = false;
 
-  // Light.prototype.clickResponse = function(){
-  // }
-  // Light.prototype.toggleLight = function(){
+    this.render().bindClick().appendTo(this.board);
+  }
 
-  // }
+  Light.prototype.render = function() {
+    this.element = $(this.template());
+    return this;
+  };
+
+  Light.prototype.rerender = function() {
+    this.element.replaceWith(this.render().bindClick().element);
+    return this;
+  };
+
+  Light.prototype.bindClick = function() {
+    this.element.find('.light')
+    .on('click', function () {
+      this.lightSwitch().rerender();
+    }.bind(this));
+
+    return this;
+  };
+
+  Light.prototype.lightSwitch = function() {
+    if(this.lit) {
+      this.lit = false;
+    } else {
+      this.lit = true;
+    }
+    return this;
+  }
+
+  Light.prototype.template = function() {
+    if (this.lit) {
+      return `
+        <div class="cell">
+          <div class="light lit">
+          </div>
+        </div>
+      `;
+    } else {
+      return `
+        <div class="cell">
+          <div class="light">
+          </div>
+        </div>
+
+      `
+    }
+  }
+
+  Light.prototype.appendTo = function(target) {
+    this.element.appendTo(target);
+    return this;
+  }
+
+
 
 
   // function gameIsWon(board){
 
   // }
 
+  // board = new Board();
   board();
 }
 
