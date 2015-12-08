c7d2789fd6a0309d77e9364757bbc72bddb4803c
Matt Ewell <matt.ewell@gmail.com>
2015-12-07 18:01:26 -0700

    Working responsive grid as POC Roll out some basic squares which are all flush with one another that will toggle an inset circle background when 'lit'

--- lights-out.js
+++ lights-out.js
@@ -1,33 +1,59 @@
-function Board() {
-  this.width = 5;
-  this.lightGrid = Array.new(width);
 
-  for(var i = 0; i < this.width; i++){
-    for(var j = 0; i < this.width; j++){
-      new Light(i, j, board);
+function main(){
+  function board() {
+  // this.width = 5;
+  // this.lightGrid = Array.new(width);
+  // this.container = $('.board');
+
+    for(var i = 0; i < 5; i++){
+      for(var j = 0; j < 5; j++){
+        // var light = new Light(i, j, board);
+        $(renderLight())
+          .find('.light')
+          .on('click', function () {
+            $(this).toggleClass('lit');
+          })
+          .end()
+          .appendTo($('.board'));
+      }
     }
   }
 
+  function renderLight(){  //temporary for testing
+    return `
+      <div class="cell">
+        <div class="light">
+        </div>
+      </div>
+    `;
+  }
+
+  // Board.prototype.countLights = function()
+  // {
+
+  // }
+
+  // function Light(x, y, board) {
+  //   this.board = board;
+  //   this.x = x;
+  //   this.y = y;
+  // }
+
+  // Light.prototype.clickResponse = function(){
+  // }
+  // Light.prototype.toggleLight = function(){
+
+  // }
+
+
+  // function gameIsWon(board){
+
+  // }
+
+  board();
 }
 
-Board.prototype.countLights = function()
-{
-
-}
-
-function Light(x, y, board) {
-  this.board = board;
-  this.x = x;
-  this.y = y;
-}
-
-Light.prototype.clickResponse = function(){
-}
-Light.prototype.toggleLight = function(){
-
-}
+$(document).ready(main);
 
 
-function gameIsWon(board){
 
-}
