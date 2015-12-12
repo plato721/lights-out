a9634e188f0ba7b462fd6d81dd47234a7b35eb66
Matt Ewell <matt.ewell@gmail.com>
2015-12-12 12:43:14 -0700

    Add incrementer to score

--- test/score-test.js
+++ test/score-test.js
@@ -4,5 +4,16 @@ const assert = chai.assert;
 var Score = require('../lib/score.js');
 
 describe('Score', function() {
-  
+  it("keeps track of moves", function() {
+    var score = new Score();
+
+    assert.equal(score.moves, 0);
+  });
+
+  it("adds a move", function() {
+    var score = new Score();
+    score.incrementMoves();
+
+    assert.equal(score.moves, 1);
+  });
 });
